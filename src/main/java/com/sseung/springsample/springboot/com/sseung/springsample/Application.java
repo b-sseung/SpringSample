882cf8a67447ddf219edcd35f33e8827df4d5af2
user <kamo0915@gmail.com>
2023-01-09 18:59:47 +0900

    230109-1

---
+++ src/main/java/com/sseung/springsample/springboot/com/sseung/springsample/Application.java
@@ -0,0 +1,11 @@
+package com.sseung.springsample.springboot.com.sseung.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
